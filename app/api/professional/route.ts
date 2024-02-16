4f389f79d6f26abc02af178d456c6029092526cf
IMvErickz <erickspy2003@gmail.com>
2024-02-16 15:42:49 -0300

    delete api folder

--- app/api/professional/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/lib/prisma"
-import { NextResponse } from "next/server"
-
-export async function GET() {
-    const professional = await prisma.professional.findMany()
-
-    return NextResponse.json({ professional })
-}
