3f8b324e6a6896eaaf61f0ce3931a2a751b9830c
IMvErickz <erickspy2003@gmail.com>
2024-02-18 16:44:16 -0300

    fix: image width

--- components/News/Apresentation.tsx
+++ components/News/Apresentation.tsx
@@ -5,7 +5,9 @@ import { DataProps } from "@/@types/News";
 export function Apresentation(props: DataProps) {
     return (
         <div className="w-full max-w-[900px] max-h-[186px] flex flex-col sm:flex-row items-center justify-center gap-y-9 sm:pt-0 pt-60 sm:gap-x-9">
-            <Image src={props.imgUrl} width={344} height={18} alt="" className="rounded" />
+            <div className="w-[544px] h-[194px] pt-6">
+                <img src={props.imgUrl} alt="" className="rounded size-auto" />
+            </div>
             <div className="sm:size-full size-[300px] flex flex-col pt-1 gap-y-1">
                 <Link href={`/news/${props.id}`} className="text-blue-450 font-bold text-2xl leading-9 hover:underline">
                     {props.title}
