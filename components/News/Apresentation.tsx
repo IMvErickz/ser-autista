0027250d2cff948cf778e5ad617d7508cb619f1e
IMvErickz <erickspy2003@gmail.com>
2024-02-18 16:46:25 -0300

    fix: size 100%

--- components/News/Apresentation.tsx
+++ components/News/Apresentation.tsx
@@ -6,7 +6,7 @@ export function Apresentation(props: DataProps) {
     return (
         <div className="w-full max-w-[900px] max-h-[186px] flex flex-col sm:flex-row items-center justify-center gap-y-9 sm:pt-0 pt-60 sm:gap-x-9">
             <div className="w-[544px] h-[194px] pt-6">
-                <img src={props.imgUrl} alt="" className="rounded size-auto" />
+                <img src={props.imgUrl} alt="" className="rounded size-[100%]" />
             </div>
             <div className="sm:size-full size-[300px] flex flex-col pt-1 gap-y-1">
                 <Link href={`/news/${props.id}`} className="text-blue-450 font-bold text-2xl leading-9 hover:underline">
